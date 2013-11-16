0db952a9e62ff3042a9d644cea4dceb253e8d137
Lio <Lio@LioAsusF8VA>
2013-11-16 16:38:10 +0100

    Initial commit codé sous Pycharm

---
+++ String/test001.py
@@ -0,0 +1,15 @@
+__author__ = 'LioAsusF8VA'
+
+print(__author__)
+print(len(__author__))
+
+#dash for repetition
+repeat = "*" * 50
+print(repeat)
+
+#if __author__ == 'lionel'
+#print("OK")
+#else
+#print("KO, le nom est,", __author__)
+#endif
+
