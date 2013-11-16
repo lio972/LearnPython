d052976298788751e4f87dc9db83290b00da81d5
Lio <Lio@LioAsusF8VA>
2013-11-16 17:15:08 +0100

    go on

---
+++ String/convertBetweenNumber&String.py
@@ -0,0 +1,5 @@
+__author__ = 'LioAsusF8VA'
+
+#name = "agent" + 1179   # provoque à la compile  ==> TypeError: Can't convert 'int' object to str implicitly
+name001 = "agent" + str(1179)
+print(name001)
