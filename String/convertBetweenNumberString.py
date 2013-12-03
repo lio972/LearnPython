b493e786d1c2a716e544e4d7b7221d297d77de08
lio <progsystem@gmail.com>
2013-12-03 19:45:15 +0100

    fff

---
+++ String/convertBetweenNumberString.py
@@ -0,0 +1,18 @@
+__author__ = 'LioAsusF8VA'
+
+#name = "agent" + 1179   # provoque à la compile  ==> TypeError: Can't convert 'int' object to str implicitly
+name001 = "agent" + str(1179)
+print(name001)
+
+print(int("112")+ int("004"))
+nom = "lionel"
+if nom =="lionel":
+    print("salut %s",nom)
+else:
+    print("salut %s",nom)
+
+K = 0
+while K <=10:
+    print("K=",K)
+    K = K + 1
+
